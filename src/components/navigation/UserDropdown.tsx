6e10881970a5ee4a67417f4bc48afb7d1ae883f0
Thibault <thibault@premieroctet.com>
2022-07-22 18:31:43 +0200

    Improve user dropdown menu

--- src/components/navigation/UserDropdown.tsx
+++ src/components/navigation/UserDropdown.tsx
@@ -10,14 +10,16 @@ import {
   Box,
 } from "@chakra-ui/react";
 import { signOut, useSession } from "next-auth/client";
-import router from "next/router";
 import { FiLogOut } from "react-icons/fi";
 import { BsBook } from "react-icons/bs";
 import { RiUserAddLine } from "react-icons/ri";
 import { HiOutlinePencilAlt } from "react-icons/hi";
+import NextLink from "next/link";
 
 const UserDropdoown = () => {
   const [session, _] = useSession();
+  const userName = session?.user?.name || session?.user?.email;
+
   return (
     <Menu>
       <MenuButton>
@@ -30,7 +32,7 @@ const UserDropdoown = () => {
       </MenuButton>
       <MenuList>
         <HStack paddingX={4} paddingY={2}>
-          <Text fontWeight="bold"> {session?.user?.name} </Text>
+          <Text fontWeight="bold"> {userName} </Text>
           {session?.user?.isAdmin && (
             <Box
               backgroundColor="secondary.400"
@@ -45,31 +47,32 @@ const UserDropdoown = () => {
             </Box>
           )}
         </HStack>
-        <MenuItem onClick={() => router.push("/admin/courses")}>
-          <HStack>
-            <Icon as={HiOutlinePencilAlt} w={6} h={6} />
-            <Text fontWeight={"500"}>Gérer les cours</Text>
-          </HStack>
-        </MenuItem>
         {session?.user?.isAdmin && (
-          <MenuItem onClick={() => router.push("/admin/users")}>
-            <HStack>
-              <Icon as={RiUserAddLine} w={6} h={6} />
-              <Text fontWeight={"500"}>Ajouter un stagiaire</Text>
-            </HStack>
-          </MenuItem>
+          <>
+            <NextLink href="/admin/courses" passHref>
+              <MenuItem
+                as="a"
+                icon={<Icon as={HiOutlinePencilAlt} w={6} h={6} />}
+              >
+                Gérer les cours
+              </MenuItem>
+            </NextLink>
+
+            <NextLink href="/admin/users" passHref>
+              <MenuItem icon={<Icon as={RiUserAddLine} w={6} h={6} />}>
+                Ajouter un stagiaire
+              </MenuItem>
+            </NextLink>
+          </>
         )}
-        <MenuItem onClick={() => router.push("/")}>
-          <HStack>
-            <Icon as={BsBook} w={6} h={6} />
-            <Text fontWeight={"500"}>Mes cours</Text>
-          </HStack>
-        </MenuItem>
-        <MenuItem onClick={() => signOut()}>
-          <HStack>
-            <Icon as={FiLogOut} w={6} h={6} />
-            <Text fontWeight={"500"}>Se déconnecter</Text>
-          </HStack>
+        <NextLink href="/" passHref>
+          <MenuItem icon={<Icon as={BsBook} w={6} h={6} />}>Mes cours</MenuItem>
+        </NextLink>
+        <MenuItem
+          onClick={() => signOut()}
+          icon={<Icon as={FiLogOut} w={6} h={6} />}
+        >
+          Se déconnecter
         </MenuItem>
       </MenuList>
     </Menu>
