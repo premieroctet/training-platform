9a5e3b6881660939f6ac213639891583a4f0b39e
shinework <adrien.baptiste@gmail.com>
2023-01-13 18:23:18 +0100

    fix: remove output tracing

--- next.config.js
+++ next.config.js
@@ -1,18 +1,20 @@
 const { withSentryConfig } = require("@sentry/nextjs");
+const removeImports = require("next-remove-imports")();
+
 const withMDX = require("@next/mdx")({
   extension: /\.(md|mdx)$/,
 });
-const removeImports = require("next-remove-imports")();
 
 const configWithMDX = withMDX({
   // Pick up MDX files in the /pages/ directory
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  outputFileTracing: false,
 });
+
 const SentryWebpackPluginOptions = {
   silent: true,
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
+
 module.exports = removeImports(
   process.env.SENTRY_AUTH_TOKEN
     ? withSentryConfig(configWithMDX, SentryWebpackPluginOptions)
