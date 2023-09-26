3ea6be4568fc54bb66ac56a5a8a14f6820af0a7d
shinework <adrien.baptiste@gmail.com>
2023-09-26 22:49:01 +0200

    feat: update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-const { withSentryConfig } = require("@sentry/nextjs");
 const withMDX = require("@next/mdx")({
   extension: /\.(md|mdx)$/,
 });
@@ -8,13 +7,5 @@ const configWithMDX = withMDX({
   // Pick up MDX files in the /pages/ directory
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 });
-const SentryWebpackPluginOptions = {
-  silent: true,
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-module.exports = removeImports(
-  process.env.SENTRY_AUTH_TOKEN
-    ? withSentryConfig(configWithMDX, SentryWebpackPluginOptions)
-    : configWithMDX
-);
+
+module.exports = removeImports(configWithMDX);
