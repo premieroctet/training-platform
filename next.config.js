680412ab7565abdb10a6984df9fdb09efdfa9b3f
Thibault Lenclos <thibault@premieroctet.com>
2024-08-29 17:53:40 +0200

    fix: setup standalone output

--- next.config.js
+++ next.config.js
@@ -2,10 +2,15 @@ const withMDX = require("@next/mdx")({
   extension: /\.(md|mdx)$/,
 });
 const removeImports = require("next-remove-imports")();
-
 const configWithMDX = withMDX({
   // Pick up MDX files in the /pages/ directory
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 });
 
-module.exports = removeImports(configWithMDX);
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  ...removeImports(configWithMDX),
+  output: "standalone",
+};
+
+module.exports = nextConfig;
