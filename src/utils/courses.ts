39edb9477695982e432ff75a375bc3d9368ab837
Thibault Lenclos <thibault@premieroctet.com>
2024-08-30 18:29:10 +0200

    fix: course cover

--- src/utils/courses.ts
+++ src/utils/courses.ts
@@ -1,6 +1,6 @@
 import { Training } from "@prisma/client";
 
 export const getCourseCover = (course: Training) => {
-  const cover = require(`../../courses/${course?.slug}/assets/cover.png`);
+  const cover = require(`${__dirname}/../../courses/${course?.slug}/assets/cover.png`);
   return encodeURI(cover.default.src);
 };
